58da507fc273bef5a51255ac7c41ba6aa9005f64
noman51 <nomanridoy998@gmail.com>
2026-01-19 23:53:48 +0600

    footer

---
+++ view/footer.php
@@ -0,0 +1,10 @@
+</div>
+
+<div class="footer" </div>
+  <div>Mobile Mart © <?= date('Y') ?> </div>
+  <div>  WebTech Project</div>
+  <div>Developed by Rony || Ashik || Noman</div>
+</div>
+
+</body>
+</html>
